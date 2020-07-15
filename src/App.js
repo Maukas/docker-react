6af4cf0670dea5b3591df3095be9ac5d560c0fc0
Damien Laffond <damien.laffond@nventive.com>
2020-07-15 10:40:35 -0400

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BIg boom big badaboom<code>src/App.js</code> and save to reload.
+          BIg boom big badaboom IN FEATURE BRANCH<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
